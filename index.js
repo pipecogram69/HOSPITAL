ddbd405662af99779e11353fd57e6d9eab61f8db
mfonsecai <102571303+mfonsecai@users.noreply.github.com>
2023-11-04 11:49:11 -0500

    :)

---
+++ index.js
@@ -0,0 +1,18 @@
+//importar libreria
+const express = require("express");
+
+
+
+//objetos para llamar los metodos de express
+const app = express();
+
+//ruta inicial
+app.get("/",function(req,res){
+    res.send("hola");
+});
+
+//configurar el puerto usado para el servidor local
+app.listen(3000,function(){
+    console.log("El servidor es http://localhost:3000");
+});
+
