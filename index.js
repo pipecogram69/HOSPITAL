a9a29ac15e4e83017d47dd31ea0c33b0bf1ae174
mfonsecai <102571303+mfonsecai@users.noreply.github.com>
2023-11-05 18:52:24 -0500

    :)

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
-//importar libreria
-const express = require("express");
+const express = require ("express");
 
-
-
-//objetos para llamar los metodos de express
 const app = express();
 
 
-app.set("view engine","css");
-
-
-app.use(express.json()); //reconocer objetos json
-app.use(express.urlencoded({extended:false})); //reconocer datos html o otra unicacion
-
-
-
-app.post("/validar", function(req,res){
-    const datos = req.body//almacenar
-    console.log(datos);
-})
-
-
-//middleware
-app.use(express.static("public"));
-
-
-
-//configurar el puerto usado para el servidor local
 app.listen(3000,function(){
-    console.log("El servidor es http://localhost:3000");
+    console.log ("Servidor creado http://localhost:3000");
 });
-
