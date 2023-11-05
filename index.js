18d3a95cf11a737a43f76faa58d4a492224f02f1
mfonsecai <102571303+mfonsecai@users.noreply.github.com>
2023-11-05 16:02:38 -0500

    public

--- index.js
+++ index.js
@@ -6,14 +6,28 @@ const express = require("express");
 //objetos para llamar los metodos de express
 const app = express();
 
-//ruta inicial
-app.get("/",function(req,res){
-    res.send("hola");
-});
+
+app.set("view engine","css");
+
+
+app.use(express.json()); //reconocer objetos json
+app.use(express.urlencoded({extended:false})); //reconocer datos html o otra unicacion
+
+
+
+app.post("/validar", function(req,res){
+    const datos = req.body//almacenar
+    console.log(datos);
+})
+
+
+//middleware
+app.use(express.static("public"));
+
+
 
 //configurar el puerto usado para el servidor local
 app.listen(3000,function(){
     console.log("El servidor es http://localhost:3000");
-    
 });
 
