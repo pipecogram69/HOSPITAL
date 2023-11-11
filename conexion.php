9b5f91deb120d0eade743f07cebfadcbf7504b34
mfonsecai <102571303+mfonsecai@users.noreply.github.com>
2023-11-11 17:26:44 -0500

    bd, boton

--- conexion.php
+++ conexion.php
@@ -1,5 +1,4 @@
 <?php
-// Conexión a la BD
 $conn = mysqli_connect("localhost", "root", "", "hospital"); 
 
 // Recibir datos del formulario usando POST
@@ -13,38 +12,44 @@ $identificacion = $_POST['id'];
 $contraseña = $_POST['con'];
 
 // Verificar si el número de identificación ya existe en la base de datos
-$sql_verificar = "SELECT * FROM registros WHERE REG_ID = '$identificacion'";
+$sql_verificar = "SELECT * FROM registros WHERE reg_id = '$identificacion'";
 $resultado_verificar = mysqli_query($conn, $sql_verificar);
 
 if(mysqli_num_rows($resultado_verificar) > 0) {
     // Si el número de identificación ya existe, mostrar un mensaje y evitar la inserción
     echo "Este número de identificación ya está registrado. Por favor, intente con otro número de identificación.";
 } else {
-    // Si el número de identificación no existe, realizar la inserción en registros
-    $sql_insertar_registro = "INSERT INTO registros(REG_NOMBRES, REG_APELLIDOS, REG_CORREO, REG_CELULAR, REG_NUM_SEG, REG_TIPO_ID, REG_ID, REG_CONTRA) 
-            VALUES ('$nombre', '$apellido', '$correo', '$celular', '$num_seguridad_social', '$tipo_identificacion', '$identificacion', '$contraseña')";
-        
-    // Ejecutar la consulta de inserción en registros
-    if(mysqli_query($conn, $sql_insertar_registro)){
-        // Mostrar mensaje de éxito
-        echo "Registro creado con éxito"; 
-        
-        // Después de crear el registro, insertarlo en la tabla de pacientes
-        $sql_insertar_paciente = "INSERT INTO pacientes(reg_id, reg_contra) 
-            VALUES ('$identificacion', '$contraseña')";
-        
-        // Ejecutar la consulta de inserción en pacientes
-        if(mysqli_query($conn, $sql_insertar_paciente)){
-            // Mostrar mensaje de éxito
-            echo "Paciente registrado con éxito"; 
-        
-            // Redireccionar 
-            header("Location: inicio sesion.html");
-        } else {
-            echo "Error al registrar paciente";
-        }
+    // Verificar requisitos de la contraseña
+    if (strlen($contraseña) < 8 || !preg_match("/[A-Z]/", $contraseña) || !preg_match("/[!@#$%^&*(),.?\":{}|<>]/", $contraseña)) {
+        // Si la contraseña no cumple con los requisitos, mostrar un mensaje de error
+        echo "La contraseña debe tener al menos 8 caracteres, una letra mayúscula y un carácter especial.";
     } else {
-        echo "Error al crear el registro";
+        // Si el número de identificación no existe y la contraseña cumple con los requisitos, realizar la inserción en registros
+        $sql_insertar_registro = "INSERT INTO registros(reg_nombres, reg_apellidos, reg_correo, reg_celular, reg_num_seg, reg_tipo_id, reg_id, reg_contra) 
+                VALUES ('$nombre', '$apellido', '$correo', '$celular', '$num_seguridad_social', '$tipo_identificacion', '$identificacion', '$contraseña')";
+            
+        // Ejecutar la consulta de inserción en registros
+        if(mysqli_query($conn, $sql_insertar_registro)){
+            // Mostrar mensaje de éxito
+            echo "Registro creado con éxito"; 
+        
+            // Después de crear el registro, insertarlo en la tabla de pacientes
+            $sql_insertar_paciente = "INSERT INTO pacientes(reg_id, reg_contra) 
+                VALUES ('$identificacion', '$contraseña')";
+            
+            // Ejecutar la consulta de inserción en pacientes
+            if(mysqli_query($conn, $sql_insertar_paciente)){
+                // Mostrar mensaje de éxito
+                echo "Paciente registrado con éxito"; 
+            
+                // Redireccionar 
+                header("Location: inicio sesion.html");
+            } else {
+                echo "Error al registrar paciente";
+            }
+        } else {
+            echo "Error al crear el registro";
+        }
     }
 }
 ?>
